02df147e40a097c08c03b086446e45eec653d8d5
Masaya Nakashima <kawa18sima@gmail.com>
2018-07-01 10:53:47 +0900

    Automatic generation of controller
    Automatic generation of controller
    Describe signup action and login action

---
+++ src/Controller/UsersController.php
@@ -0,0 +1,33 @@
+<?php
+namespace App\Controller;
+
+use App\Controller\AppController;
+
+/**
+ * Users Controller
+ *
+ * @property \App\Model\Table\UsersTable $Users
+ *
+ * @method \App\Model\Entity\User[]|\Cake\Datasource\ResultSetInterface paginate($object = null, array $settings = [])
+ */
+class UsersController extends AppController
+{
+
+    public function signup()
+    {
+        $user = $this->Users->newEntity();
+        if ($this->request->is('post')) {
+            $user = $this->Users->patchEntity($user, $this->request->getData());
+            if ($this->Users->save($user)) {
+                $this->Flash->success(__('The user has been saved.'));
+            }
+            $this->Flash->error(__('The user could not be saved. Please, try again.'));
+        }
+        $this->set(compact('user'));
+    }
+
+    public function login(){
+
+    }
+
+}
