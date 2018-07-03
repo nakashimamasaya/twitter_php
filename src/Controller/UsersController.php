f95ae4aef3c6c3cbf07e004a0555f0d8a7645c20
Masaya Nakashima <kawa18sima@gmail.com>
2018-07-03 15:29:25 +0900

    Implementation of login and logout processing
    I did the following.
    
    ・Implementation of login function.
    ・Redirect processing while logging in (login, singup).
    ・Implementation of logout processing.

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -2,6 +2,7 @@
 namespace App\Controller;
 
 use App\Controller\AppController;
+use Cake\Event\Event;
 
 /**
  * Users Controller
@@ -12,22 +13,54 @@ use App\Controller\AppController;
  */
 class UsersController extends AppController
 {
+    public function beforeFilter(Event $event)
+    {
+        $this->set('user', $this->Auth->user());
+    }
+
+    public function index(){
+    }
 
     public function signup()
     {
-        $user = $this->Users->newEntity();
-        if ($this->request->is('post')) {
-            $user = $this->Users->patchEntity($user, $this->request->getData());
-            if ($this->Users->save($user)) {
-                $this->set('user', $user);
-                $this->render('complete');
-            }
+        $user = $this->Auth->user();
+        if(isset($user)){
+            $this->redirect(['controller'=>'Users','action'=>'index']);
         }
-        $this->set(compact('user'));
+        else{
+            $user = $this->Users->newEntity();
+            if ($this->request->is('post')) {
+                $user = $this->Users->patchEntity($user, $this->request->getData());
+                if ($this->Users->save($user)) {
+                    $this->set('user', $user);
+                    $this->render('complete');
+                }
+            }
+            $this->set(compact('user'));
+        }
     }
 
     public function login(){
+        $user = $this->Auth->user();
+        if(isset($user)){
+            $this->redirect(['controller'=>'Users','action'=>'index']);
+        }
+        else{
+            if ($this->request->is('post')) {
+                $user = $this->Auth->identify();
+                if ($user) {
+                    $this->Auth->setUser($user);
+                    return $this->redirect($this->Auth->redirectUrl());
+                }
+                $this->Flash->error(__('ユーザ名もしくはパスワードが間違っています'));
+            }
 
+        }
+    }
+
+    public function logout(){
+        $this->Flash->success('ログアウトしました');
+        return $this->redirect($this->Auth->logout());
     }
 
 }
