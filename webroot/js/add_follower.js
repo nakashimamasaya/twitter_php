5e7702555687b7c69d867555416aafff70dc30be
Masaya Nakashima <kawa18sima@gmail.com>
2018-07-19 03:58:31 +0900

    Handling error
    add_follower.js
    
    Change the return value of ajax to json
    Fix error in redraw description of error

--- webroot/js/add_follower.js
+++ webroot/js/add_follower.js
@@ -5,13 +5,22 @@ $(document).ready(function(){
         url: "/following/add",
         type: "POST",
         data: {id: id},
-        dataType: "text"
+        dataType: "json"
     }).done(function (response) {
+        if(response["result"] === "OK"){
+            $(this).html((index, element)=>{
+                $(`.follow_${id}`).replaceWith('<h3>フォローしました</h3>');
+            });
+        }
+        else if (response["result"] === "NO"){
+            $(this).html((index, element)=>{
+                $(`#${id}`).prev('.error_message').replaceWith('<p class="error">フォローに失敗しました</p>');
+            });
+        }
+    }).fail(function (response) {
         $(this).html((index, element)=>{
-            $(`.follow_${id}`).replaceWith('<h3>フォローしました</h3>');
+            $(`#${id}`).prev('.error_message').replaceWith('<p class="error">フォローに失敗しました</p>');
         });
-    }).fail(function () {
-        $(`#error_${id}`).html('フォローに失敗しました');
     });
   });
 });
